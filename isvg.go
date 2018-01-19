d3f6c53f14b42c5bca9a89ffad5a71d2b00e9c1a
shiyongbin <shiyongbin@ezbuy.com>
2018-01-19 13:10:12 +0800

    isvg

---
+++ isvg.go
@@ -0,0 +1,18 @@
+package isvg
+
+import (
+	"os/exec"
+
+	"github.com/toukii/bytes"
+)
+
+func Decode(bs []byte) ([]byte, error) {
+	cmd := exec.Command("rsvg-convert")
+	w := bytes.NewWriter(make([]byte, 0, 1024))
+	cmd.Stdout = w
+	cmd.Stdin = bytes.NewReader(bs)
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+	return w.Bytes(), nil
+}
